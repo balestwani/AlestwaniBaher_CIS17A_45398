88525ebdc076f12398ed60921bda302969778d10
balestwani <baher.e2004@gmail.com>
2021-07-15 09:33:56 -0700

    Signed-off-by: balestwani <baher.e2004@gmail.com>

---
+++ Lab/IntroLab/HelloWorld Lab/main.cpp
@@ -0,0 +1,20 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Baher Alestwani
+ * Created on July 14, 2021, 4:10 PM
+ * Purpose: Hello World Program Intro Lab
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    cout << "Hello World!"<< endl;
+    
+    return 0;
+}
+
